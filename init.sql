da758f1f5c9933a7c9482dbf7f4cb2328f76d3e5
Maruf Tuhin <maruf.2hin@gmail.com>
2018-01-24 14:58:48 +0600

    Update SQL script for mysql
    > Creates a table 'kubedb_table' inside mysql database
    > Inserts three demo data (row) in this table

--- init.sql
+++ init.sql
@@ -1,14 +1,31 @@
-CREATE DATABASE IF NOT EXISTS test;
+USE `mysql`;
 
-use test;
+DROP TABLE IF EXISTS
+    `kubedb_table`;
 
-CREATE TABLE IF NOT EXISTS Dashboard(
-	id int,
-    version int,
-    slug varchar(255),
-    title varchar(255),
-    data text NOT NULL,
-    org_id int NOT NULL
-)
+CREATE TABLE `kubedb_table`(
+    `id` BIGINT(20) NOT NULL,
+    `name` VARCHAR(255) DEFAULT NULL
+);
 
+--
+-- Dumping data for table `kubedb_table`
+--
 
+INSERT INTO `kubedb_table`(`id`, `name`)
+VALUES(1, 'name1'),(2, 'name2'),(3, 'name3');
+
+--
+-- Indexes for table `kubedb_table`
+--
+
+ALTER TABLE
+    `kubedb_table` ADD PRIMARY KEY(`id`);
+
+--
+-- AUTO_INCREMENT for table `kubedb_table`
+--
+
+ALTER TABLE
+    `kubedb_table` MODIFY `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+    AUTO_INCREMENT = 4;
