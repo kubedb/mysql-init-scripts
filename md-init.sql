03459831612d714e0557e3317a6acafa618800b4
Alif Biswas <alif@appscode.com>
2021-02-16 16:18:57 +0600

    updated table name Signed-off-by: Alif Biswas <alif@appscode.com>

--- md-init.sql
+++ md-init.sql
@@ -3,29 +3,29 @@ USE `mysql`;
 DROP TABLE IF EXISTS
     `kubedb_table_m_d`;
 
-CREATE TABLE `kubedb_table`(
+CREATE TABLE `kubedb_table_m_d`(
     `id` BIGINT(20) NOT NULL,
     `name` VARCHAR(255) DEFAULT NULL
 );
 
 --
--- Dumping data for table `kubedb_table`
+-- Dumping data for table `kubedb_table_m_d`
 --
 
-INSERT INTO `kubedb_table`(`id`, `name`)
+INSERT INTO `kubedb_table_m_d`(`id`, `name`)
 VALUES(1, 'name1'),(2, 'name2'),(3, 'name3');
 
 --
--- Indexes for table `kubedb_table`
+-- Indexes for table `kubedb_table_m_d`
 --
 
 ALTER TABLE
-    `kubedb_table` ADD PRIMARY KEY(`id`);
+    `kubedb_table_m_d` ADD PRIMARY KEY(`id`);
 
 --
--- AUTO_INCREMENT for table `kubedb_table`
+-- AUTO_INCREMENT for table `kubedb_table_m_d`
 --
 
 ALTER TABLE
-    `kubedb_table` MODIFY `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+    `kubedb_table_m_d` MODIFY `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
     AUTO_INCREMENT = 4;
