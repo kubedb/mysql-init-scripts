a12d3ed96e0526fa07353aca458619aef73a32c3
Alif Biswas <alif@appscode.com>
2021-02-16 16:12:40 +0600

    added mariadb init script Signed-off-by: Alif Biswas <alif@appscode.com>

---
+++ md-init.sql
@@ -0,0 +1,31 @@
+USE `mysql`;
+
+DROP TABLE IF EXISTS
+    `kubedb_table_m_d`;
+
+CREATE TABLE `kubedb_table`(
+    `id` BIGINT(20) NOT NULL,
+    `name` VARCHAR(255) DEFAULT NULL
+);
+
+--
+-- Dumping data for table `kubedb_table`
+--
+
+INSERT INTO `kubedb_table`(`id`, `name`)
+VALUES(1, 'name1'),(2, 'name2'),(3, 'name3');
+
+--
+-- Indexes for table `kubedb_table`
+--
+
+ALTER TABLE
+    `kubedb_table` ADD PRIMARY KEY(`id`);
+
+--
+-- AUTO_INCREMENT for table `kubedb_table`
+--
+
+ALTER TABLE
+    `kubedb_table` MODIFY `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+    AUTO_INCREMENT = 4;
